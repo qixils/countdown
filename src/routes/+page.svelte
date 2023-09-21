a1f2eb0d2642a35e0e541d9b174aeaf440d4c982
Lexi Larkin <lexi@qixils.dev>
2023-09-21 01:00:15 -0400

    feat: home page

---
+++ src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<svelte:head>
+    <title>Countdown Timer Setup</title>
+    <meta property="og:title" content="Countdown Timer Setup">
+    <meta property="og:description" content="Setup for a simple search parameter driven countdown timer.">
+    <meta name="description" content="Setup for a simple search parameter driven countdown timer.">
+</svelte:head>
+
+<div>
+    <p>Hi.</p>
+    <p>There should be a setup wizard here.</p>
+    <p>But there isn't.</p>
+    <p>
+        Instead I'll just give you
+        <a href="/timer?duration=300">this link</a>
+        and tell you that the available search parameters are
+        <code>duration</code>,
+        <code>to</code>,
+        <code>color</code>,
+        <code>font</code>,
+        and <code>size</code>.
+    </p>
+</div>
